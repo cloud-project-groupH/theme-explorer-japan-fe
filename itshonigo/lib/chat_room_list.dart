511e27f9339d043f5a2b55e2029061345b8ef798
PinkAnimal <jipegalre@gmail.com>
2024-11-30 13:40:37 +0900

    채팅방 목록 페이지 listview 수정

--- itshonigo/lib/chat_room_list.dart
+++ itshonigo/lib/chat_room_list.dart
@@ -42,6 +42,35 @@ class _ChatRoomListWidgetState extends State<ChatRoomListWidget> {
 
   @override
   Widget build(BuildContext context) {
+    final chatRooms = [
+      {
+        "name": "채팅방 이름",
+        "image": "assets/images/choco.png",
+        "recentDate": "최근 채팅 날짜",
+        "recent": "최근 채팅",
+        "number": "안 읽은 채팅 개수"
+
+
+      },
+      {
+        "name": "채팅방 이름",
+        "image": "assets/images/choco.png",
+        "recentDate": "최근 채팅 날짜",
+        "recent": "최근 채팅",
+        "number": "안 읽은 채팅 개수"
+
+
+      },
+      {
+        "name": "채팅방 이름",
+        "image": "assets/images/choco.png",
+        "recentDate": "최근 채팅 날짜",
+        "recent": "최근 채팅",
+        "number": "안 읽은 채팅 개수"
+
+
+      }
+      ];
     return GestureDetector(
       onTap: () => FocusScope.of(context).unfocus(),
       child: Scaffold(
@@ -80,202 +109,24 @@ class _ChatRoomListWidgetState extends State<ChatRoomListWidget> {
             mainAxisSize: MainAxisSize.max,
             children: [
               Expanded(
-                child: ListView(
+                child: ListView.separated(
                   padding: EdgeInsets.zero,
                   shrinkWrap: true,
                   scrollDirection: Axis.vertical,
-                  children: [
-                    Container(
-                      width: MediaQuery.sizeOf(context).width,
-                      height: 100,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                      ),
-                      child: Padding(
-                        padding: EdgeInsets.all(8),
-                        child: Row(
-                          mainAxisSize: MainAxisSize.max,
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Container(
-                              width: 50,
-                              height: 50,
-                              clipBehavior: Clip.antiAlias,
-                              decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                              ),
-                              child: Image.asset(
-                                'assets/images/choco.png',
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                            Expanded(
-                              child: Padding(
-                                padding: EdgeInsets.all(4),
-                                child: Column(
-                                  mainAxisSize: MainAxisSize.max,
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceEvenly,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Expanded(
-                                      child: Row(
-                                        mainAxisSize: MainAxisSize.max,
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.spaceBetween,
-                                        children: [
-                                          Text(
-                                            '채팅방이름',
-                                            style: TextStyle(
-                                                  fontFamily: 'Inter Tight',
-                                                  color: Colors.black,
-                                                  fontSize: 16,
-                                                  letterSpacing: 0.0,
-                                                ),
-                                          ),
-                                          Text(
-                                            '최근 채팅 날짜',
-                                            style: TextStyle(
-                                                  fontFamily: 'Inter Tight',
-                                                  color: Colors.black,
-                                                  fontSize: 14,
-                                                  letterSpacing: 0.0,
-                                                ),
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                    Expanded(
-                                      child: Row(
-                                        mainAxisSize: MainAxisSize.max,
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.spaceBetween,
-                                        children: [
-                                          Text(
-                                            '최근 채팅',
-                                            style: TextStyle(
-                                                  fontFamily: 'Inter Tight',
-                                                  color: Colors.black,
-                                                  fontSize: 12,
-                                                  letterSpacing: 0.0,
-                                                ),
-                                          ),
-                                          Text(
-                                            '안읽은 채팅 갯수',
-                                            style: TextStyle(
-                                                  fontFamily: 'Inter Tight',
-                                                  color: Colors.orange,
-                                                  fontSize: 12,
-                                                  letterSpacing: 0.0,
-                                                ),
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    Container(
-                      width: MediaQuery.sizeOf(context).width,
-                      height: 100,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                      ),
-                      child: Padding(
-                        padding: EdgeInsets.all(8),
-                        child: Row(
-                          mainAxisSize: MainAxisSize.max,
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Container(
-                              width: 50,
-                              height: 50,
-                              clipBehavior: Clip.antiAlias,
-                              decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                              ),
-                              child: Image.asset(
-                                'assets/images/choco.png',
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                            Expanded(
-                              child: Padding(
-                                padding: EdgeInsets.all(4),
-                                child: Column(
-                                  mainAxisSize: MainAxisSize.max,
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceEvenly,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Expanded(
-                                      child: Row(
-                                        mainAxisSize: MainAxisSize.max,
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.spaceBetween,
-                                        children: [
-                                          Text(
-                                            '채팅방이름',
-                                            style: TextStyle(
-                                                  fontFamily: 'Inter Tight',
-                                                  color: Colors.black,
-                                                  fontSize: 16,
-                                                  letterSpacing: 0.0,
-                                                ),
-                                          ),
-                                          Text(
-                                            '최근 채팅 날짜',
-                                            style: TextStyle(
-                                                  fontFamily: 'Inter Tight',
-                                                  color: Colors.black,
-                                                  fontSize: 14,
-                                                  letterSpacing: 0.0,
-                                                ),
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                    Expanded(
-                                      child: Row(
-                                        mainAxisSize: MainAxisSize.max,
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.spaceBetween,
-                                        children: [
-                                          Text(
-                                            '최근 채팅',
-                                            style: TextStyle(
-                                                  fontFamily: 'Inter Tight',
-                                                  color: Colors.black,
-                                                  fontSize: 12,
-                                                  letterSpacing: 0.0,
-                                                ),
-                                          ),
-                                          Text(
-                                            '안읽은 채팅 갯수',
-                                            style: TextStyle(
-                                                  fontFamily: 'Inter Tight',
-                                                  color: Colors.orange,
-                                                  fontSize: 12,
-                                                  letterSpacing: 0.0,
-                                                ),
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
+                  itemCount: chatRooms.length,
+                  separatorBuilder: (BuildContext context, int index) => const Divider(), 
+                  itemBuilder: (BuildContext context, int index){
+                    return chatRoomEntity(
+                      name:chatRooms[index]["name"].toString(),
+                      image:chatRooms[index]["image"].toString(),
+                      recentDate:chatRooms[index]["recentDate"].toString(),
+                      recent:chatRooms[index]["recent"].toString(),
+                      number:chatRooms[index]["number"].toString(),
+                    );
+                  },
+
+                 
+                  
                 ),
               ),
             ],
@@ -284,4 +135,104 @@ class _ChatRoomListWidgetState extends State<ChatRoomListWidget> {
       ),
     );
   }
+  Container chatRoomEntity({String image = 'assets/images/choco.png', String name = '채팅방 이름', String recentDate = '최근 채팅 날짜',String recent = '최근 채팅', String number = '안읽은 채팅 개수'}){
+
+  return Container(
+                      width: MediaQuery.sizeOf(context).width,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        color: Color(0xffF1F4F8),
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.all(8),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Container(
+                              width: 50,
+                              height: 50,
+                              clipBehavior: Clip.antiAlias,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                              ),
+                              child: Image.asset(
+                                image,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            Expanded(
+                              child: Padding(
+                                padding: EdgeInsets.all(4),
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.max,
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Expanded(
+                                      child: Row(
+                                        mainAxisSize: MainAxisSize.max,
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          Text(
+                                            name,
+                                            style: TextStyle(
+                                                  fontFamily: 'Inter Tight',
+                                                  color: Colors.black,
+                                                  fontSize: 16,
+                                                  letterSpacing: 0.0,
+                                                ),
+                                          ),
+                                          Text(
+                                            recentDate,
+                                            style: TextStyle(
+                                                  fontFamily: 'Inter Tight',
+                                                  color: Colors.black,
+                                                  fontSize: 14,
+                                                  letterSpacing: 0.0,
+                                                ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                    Expanded(
+                                      child: Row(
+                                        mainAxisSize: MainAxisSize.max,
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          Text(
+                                            recent,
+                                            style: TextStyle(
+                                                  fontFamily: 'Inter Tight',
+                                                  color: Colors.black,
+                                                  fontSize: 12,
+                                                  letterSpacing: 0.0,
+                                                ),
+                                          ),
+                                          Text(
+                                            number,
+                                            style: TextStyle(
+                                                  fontFamily: 'Inter Tight',
+                                                  color: Colors.orange,
+                                                  fontSize: 12,
+                                                  letterSpacing: 0.0,
+                                                ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+  } 
 }
+
+
